54345df7badc1194226d07e468ff7fdab46a4c4f
Francisco Barrios <shighetari@gmail.com>
2020-07-28 20:58:25 -0400

    added knex clean up for db

--- data/seeds/001.js
+++ data/seeds/001.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('Users','Recipes','Ingredients','Instructions','RecipeServices').truncate()
+  return knex('Users','Recipes','Ingredients','Instructions','RecipeServices')
     .then(function () {
       // Inserts seed entries
       return knex('Users').insert([
