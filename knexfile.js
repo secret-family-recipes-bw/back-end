d1118fdf27462a90d8defebec97f2ddff7df2c38
Francisco Barrios <shighetari@gmail.com>
2020-07-28 20:33:22 -0400

    testing

--- knexfile.js
+++ knexfile.js
@@ -2,51 +2,49 @@ const pgConnection = process.env.DATABASE_URL || "postgresql://postgres@localhos
 
 module.exports = {
   //Development
-    development: {
-        client: "sqlite3",
-        useNullAsDefault: true,
-        connection: {
-            filename: "./data/recipes.db3",
-        },
-         // needed when using foreign keys
-        pool: {
-          afterCreate: (conn, done) => {
-             // runs after a connection is made to the sqlite engine
-            conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
-          },
-        },
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
-    },
-//Testing
-    testing: {
-        client: "sqlite3",
-        connection: {
-            filename: "./data/test.db3",
-        },
-        useNullAsDefault: true,
-        migrations: {
-            directory: "./data/migrations",
-        },
-        seeds: {
-            directory: "./data/seeds",
-        },
-    },
-
-
-   // Heroku
-   production: {
-    client: "pg",
+  development: {
+    client: "sqlite3",
     useNullAsDefault: true,
+    connection: {
+      filename: "./data/recipes.db3",
+    },
+    // needed when using foreign keys
+    pool: {
+      afterCreate: (conn, done) => {
+        // runs after a connection is made to the sqlite engine
+        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+      },
+    },
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+  //Testing
+  testing: {
+    client: "sqlite3",
+    connection: {
+      filename: "./data/test.db3",
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+
+
+  // Heroku
+  production: {
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
-      min:2,
-      max:10
-      },
+      min: 2,
+      max: 10
     },
     migrations: {
       directory: "./data/migrations",
@@ -56,4 +54,5 @@ module.exports = {
       directory: "./data/seeds",
     },
   }
+}
 
