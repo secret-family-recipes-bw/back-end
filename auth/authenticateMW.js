39b733ff0345d84fde063e870bd116bb5bd766b5
Francisco Barrios <shighetari@gmail.com>
2020-07-30 14:13:34 -0400

    implemented auth middleare, token now required to view recipe end points. Working on testing for the rest of this

---
+++ auth/authenticateMW.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken")
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization
+    const secret = 'is it secret, is it safe?'
+
+    if (token) {
+        jwt.verify(token, secret, (err, decodedToken) => {
+            if (err) {
+                // something wrong with the token
+                res.status(401).json({ you: "can't touch this!" });
+            } else {
+                // token is good we can see the data inside the decodedToken
+                req.jwt = decodedToken;
+                next();
+            }
+        })
+    } else {
+        res.status(401).json({ you: "shall not pass!" });
+    }
+};
